69314b31b5241b8c3041d9d5e9ae20c609bdd265
LindsayPeltier <lindsaypeltier@gmail.com>
2019-11-04 05:39:25 -0800

    Challenges 1 and 2

--- code-challenges/array-string-methods/challenges-05.test.js
+++ code-challenges/array-string-methods/challenges-05.test.js
@@ -15,7 +15,11 @@ For example, if the input is 'Welcome', the output will be:
 
 const howMuchPencil = (str) => {
   let result = [];
-  // Solution code here...
+
+  for (let i = 0; i <= str.length; i++) {
+    result.push(str.slice(i));
+  }
+
   return result;
 };
 
@@ -28,7 +32,7 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  return arr.split('');
 };
 
 
